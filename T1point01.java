688b840e20cfc3cb3c3c503ee379992407407ccb
Ediputrayasa <101324946+Ediputrayasa@users.noreply.github.com>
2022-04-07 20:04:29 +0800

    Edi Putrayasa - Tugas 01

---
+++ T1point01.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1point01;
+public class T1point01 {
+
+    public static void main(String[] args) {
+          // TODO code application logic here
+        String[][] data = {
+                {"Adi", "Mika", "Budi", "Bunga"},
+                {"Teknik Informatika", "Sistem Komputer"}
+                
+                };
+        int []x = new int[4];
+                x[0]=127647;
+                x[1]=129883;
+                x[2]=120495;
+                x[3]=123489;
+    
+    System.out.println("Data Mahasiswa : "+(data[0][0])+" "+x[0]+" "+data[1][0]);
+    System.out.println("Data Mahasiswa : "+(data[0][1])+" "+x[1]+" "+data[1][1]);
+    System.out.println("Data Mahasiswa : "+(data[0][2])+" "+x[2]+" "+data[1][1]);
+    System.out.println("Data Mahasiswa : "+(data[0][3])+" "+x[3]+" "+data[1][0]);
+    }
+    
+   
+    
+}
---
+++ T1point02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1point02;
+public class T1point02 {
+    public static void main(String[] args) {
+       
+        String[]x= new String[5];
+        x[0]=("Edi");
+        x[1]=("Putri");
+        x[2]=("Budi");
+        x[3]=("Ayu");
+        x[4]=("Mika");
+        
+        System.out.println(x[0]);
+        System.out.println(x[1]);
+        System.out.println(x[2]);
+        System.out.println(x[3]);
+        System.out.println(x[4]);
+    }
+}
